33bf50b4a455580cd449a136f0ef68915c219510
Jason <jrosasrjf@gmail.com>
2021-05-17 23:28:33 -0500

    required basic routes in index

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+// This brings in the api routes together <3 
+const router = require("express").Router();
+
+const commentRoutes = require("./comment-routes");
+const postRoutes = require("./post-routes");
+const userRoutes = require("./user-routes");
