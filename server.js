67624ee32e1bc432346d781d002325fd8b104b29
Jason <jrosasrjf@gmail.com>
2021-05-17 23:05:58 -0500

    added in port information to the server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const express = require("express");
 const handleBars = require("express-handlebars");
 const session = require("express-session");
 const path = require("path");
+
+// const sequelize
+const SequelizeStore = require("connect-session-sequalize")(session.Store);
+
+// express server settings (The framework)
+const app = express();
+const PORT = process.env.PORT || 3008; //Allows for connection to the server.
+
