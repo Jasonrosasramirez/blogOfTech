38ebeed31153594a82e92430fb6cd98ca6af0449
Jason <jrosasrjf@gmail.com>
2021-05-17 22:59:14 -0500

    added in basic requests

---
+++ server.js
@@ -0,0 +1,5 @@
+// requiring basic info
+const express = require("express");
+const handleBars = require("express-handlebars");
+const session = require("express-session");
+const path = require("path");
