e7d6ac8d8f8fdb7e298440bdf7159098d22c3cc8
Jason <jrosasrjf@gmail.com>
2021-05-19 23:07:10 -0500

    filled the connection file with .env data

---
+++ config/config.js
@@ -0,0 +1,22 @@
+/*  creates the teather for the myDQL databases  */ 
+
+// imports the sequalize library
+const Sequelize = require("sequelize"); 
+require("dotenv").config(); // imports DB values from the .env file. 
+
+const sequalize = new Sequelize(
+    // DB_ values from .env file
+    
+    process.env.DB_NAME, // will be TechBlogger_DB. 
+    process.env.DB_USER, // will be root. 
+    process.env.DB_PASSWORD, 
+
+    {
+        host: "localhost",
+        dialect: "mysql", 
+        port: 3306, // unique port for db. 
+            // server is using 3008. 
+    }
+); 
+
+module.export = sequalize; 
