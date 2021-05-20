6e0b06c8f9dc8d20bb1a90fc05d1881da77d8986
Jason <jrosasrjf@gmail.com>
2021-05-19 22:41:30 -0500

    finished the index file requirements

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 
 // referencing the siblings files and api folder
-const apiIndex = require("./api/index.js"); // this file references all of the api folder files 
+const apiIndexRoutes = require("./api/index.js"); // this file references all of the api folder files 
 const dashboardRoutes = require("./dashboard-routes");
 const homeRoutes = require("./home-routes.js");
 
+// routing use
+router.use("/api", apiIndexRoutes);
+router.use("/dashboard", dashboardRoutes); 
+router.use("/", homeRoutes); // sets the home route for the browser.  
+
+// exporting the file use 
+module.exports = router;
