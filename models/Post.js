3cb5876fcd9fa1690f938dde8d6b168ca26c6411
Jason <jrosasrjf@gmail.com>
2021-05-20 22:52:14 -0500

    added in the basics to the Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,5 @@
 const sequelize = require('../config/connection.js'); // the exported sequalize class from connection.js
 const { Sequelized, Model, Datatypes } = require("sequelize");
 
+class Post extends Model {}
+
