5ed8a9cdf6f1aff73fa7a3cc883c9d8afc7de30f
Jason <jrosasrjf@gmail.com>
2021-05-20 23:01:39 -0500

    added in basics for User.js based on Comment js with tutor

--- models/User.js
+++ models/User.js
@@ -1,1 +1,6 @@
 const sequelize = require('../config/connection.js'); // the exported sequalize class from connection.js
+const { Model, DataTypes } = require("sequelize"); 
+const bcrypt = require("bcrypt"); // hashng for password security platform 
+
+
+
