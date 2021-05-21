5a72ace29e49dea0c454d0c57a1991950ec5190d
Jason <jrosasrjf@gmail.com>
2021-05-20 22:44:20 -0500

    started filling in the index for models

--- models/Comment.js
+++ models/Comment.js
@@ -1,2 +1,22 @@
 const sequelize = require('../config/connection.js'); // the exported sequalize class from connection.js
 
+const { Sequelized, Model, Datatypes } = require("sequelize");
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        body: {
+            type: Datatypes.STRING,
+            allowNull: false
+        }
+
+    },
+
+    {
+        sequelize
+    }
+);
+
+// exporting to be used in index in modules folder 
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,21 @@
 /* This index sccipt brings the models working together <3 */
 
 // reference the sibling files. - Each file must have an export. 
-const Comment = require("./Comment.js");
-const Post = require("./Post.js");
-const User = require("./User.js");
+const Comment = require("./Comment");
+const Post = require("./Post");
+const User = require("./User");
 
 
 /* What are we doing with the models */
+Post.belongsTo(User, {
+    foreignKey: "userId", 
+    onDelete: "CASCADE"
+}); 
+
+Post.hasMany(Comment, {
+    foreignKey: "postId", 
+    onDelete: "CASCADE"
+}); 
 
 // will be used in schema - mySQL - need to create schema first
 
