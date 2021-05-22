c91edf6062bf88e3cb353a4bcf3380132abc4d0e
Jason <jrosasrjf@gmail.com>
2021-05-21 21:31:52 -0500

    added in comments to the auth file

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,8 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.userId) {
-        res.redirect("/login"); 
+    if (!req.session.userId) {  // if the request came back empty. 
+        res.redirect("/login"); // go to the login page if it was missing
     }   else {
-        next();
+        next(); // if all is well, then it will contunue to the next session 
     }
-} ; 
+}; 
 
