c01ba6be346147d8e45b4fdf2d9256cf26350095
Jason <jrosasrjf@gmail.com>
2021-05-21 21:39:43 -0500

    filled in helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 const helpers = {
     format_date: date => {
-        
+        return `${date.getMonth() + 1} / ${date.getDate()} / ${date.getFullYear()}`;
     }
-}
+}; 
+
+// exporting helper
+module.exports = helpers; 
