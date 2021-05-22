2813ec88a27019f0627441ab17df127fee7ddc08
Jason <jrosasrjf@gmail.com>
2021-05-22 01:21:31 -0500

    added information to the id section

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {Post} = require("../../models/index.js"); // referencing the user model
-const withAuth = require("../../utils/auth");
+const withAuth = require("../../utils/auth.js");
 
 // Home :D I'm Home :D 
 router.post("/", withAuth, async (req, res) => {
@@ -12,6 +12,22 @@ router.post("/", withAuth, async (req, res) => {
 
     } catch (err) {
         res.status(500).json(err); // if there is an error, it will be displayed in json format.
+    }
+
+}); 
+
+// id section 
+router.put("/:id", withAuth, async (req, res) =>{
+    try {
+        // if all goes well. 
+        const [intendedRows] = await Post.update(req.body, {
+            where: { // used to describe the scope of search. 
+                id: req.params.id,
+            }, 
+        }); 
+
+    } catch (err) {
+        res.status(500).json(err); // displays the error as a json format
     }
 
 }); 
