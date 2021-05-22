853efd2e85c1c03f1aa7f34299cb6445e0f2bb81
Jason <jrosasrjf@gmail.com>
2021-05-21 21:58:37 -0500

    added in try and catch template

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,22 @@ const router = require('express').Router();
 const { Post } = require('../models');
 const withAuth = require('../utils/auth.js');
 
+router.get("/", withAuth, async (req, res) => {
+    try {
+        // best case scenario
+        const tweetPost = await Post.findAll({
+            where: {
+                userId: req.session.userId,                
+            }, 
+        }); 
+
+        const posts = tweetPost.map((post) => post.get({ plain: true }));
+    
+    
+    
+    
+    } catch (err) {
+        res.redirect("login");
+    }
+
+})
