10c4bbfa419ab2b39406cbc54dd8f60e76712a31
Jason <jrosasrjf@gmail.com>
2021-05-22 00:12:25 -0500

    added in basic try catch structure in the home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,30 @@
 const router = require("express").Router();
-const {Comment, Post, User} = require("../models"); // just the models folder
+const {Comment, Post, User} = require("../models/"); // just the models folder
+
+// home 
+router.get("/", async (req, res) => {
+    try {
+        const tweetPost = await Post.findAll({
+            include: [User], 
+        }); 
+
+        const posts = tweetPost.map((post) => post.get({ plain: true }));
+    
+    }
+
+    catch (err) {
+        console.log("error on home routes");
+    }
+
+}); 
+
+
+
+
+
+
+
+
+
+// exporting file 
+module.exports = router; 
