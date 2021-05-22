709cb270c62d1124013116c862cd5ca1152049e6
Jason <jrosasrjf@gmail.com>
2021-05-21 20:40:44 -0500

    added in user int section to user.Js

--- models/User.js
+++ models/User.js
@@ -2,5 +2,26 @@ const sequelize = require('../config/connection.js'); // the exported sequalize 
 const { Model, DataTypes } = require("sequelize"); 
 const bcrypt = require("bcrypt"); // hashng for password security platform 
 
+class User extends Model {
+    passwordVerification(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.loginPassword);
 
+        /*
+        . bcrypt for secure password.
+        . this. is used for creating this instance of a loginPassword.
+            . Will be unique per object creation.
+        .   
+        .
+        */  
+    }
+}
 
+User.init({
+    id: {
+        type: DataTypes.INTEGER, 
+        allowNull: false, 
+        primaryKey: true, 
+        autoIncrement: true
+    }, 
+
+})
