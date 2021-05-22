7095b08cc0e05a3702c74b33bc3d49e8794ed58c
Jason <jrosasrjf@gmail.com>
2021-05-22 00:59:26 -0500

    filled in try catch template

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,17 @@
-const router = require('express').Router();
-const { User } = require('../../models'); // referencing the user model
+const router = require("express").Router();
+const { User } = require("../../models/"); // referencing the user model
 const withAuth = require("../../utils/auth");
 
+// Home :D I'm Home :D 
+router.post("/", withAuth, async (req, res) => {
+    const body = req.body;
+
+    try {
+        const newPost = await Post.create({ ... body, userId: req.session.userId});
+        res.json(newPost); // displays the variable above as a json format. 
+
+    } catch (err) {
+        res.status(500).json(err); // if there is an error, it will be displayed in json format.
+    }
+
+}); 
