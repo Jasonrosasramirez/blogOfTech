889e124c84ebaeabef7149fecfe291e8e60bca3f
Jason <jrosasrjf@gmail.com>
2021-05-22 00:18:39 -0500

    added in the login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,16 +9,24 @@ router.get("/", async (req, res) => {
         }); 
 
         const posts = tweetPost.map((post) => post.get({ plain: true }));
+        res.render("all-post", {posts}); 
     
-    }
-
-    catch (err) {
-        console.log("error on home routes");
+    }   catch (err) {
+            console.log("error on home routes /home");
+            res.status(500).json(err);
     }
 
 }); 
 
+// login here :D 
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/"); // ET phone home.  
+        return; // comes out. 
+    }
 
+    res.render("login"); // renders the page in response. 
+});
 
 
 
