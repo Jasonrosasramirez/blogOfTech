15a1e4e225d02a3b23907de110323a80c661b4c1
Jason <jrosasrjf@gmail.com>
2021-05-26 23:57:58 -0500

    added in basic elements of the commentScript through DOM navigation

---
+++ public/js/commentScript.js
@@ -0,0 +1,13 @@
+const commentHandler = async function (event) {
+    event.preventDefault(); // prevents the default state 
+
+    // navigating through the DOM
+    const postID = document.querySelector("input[name='post-id']").value;
+    const postTextArea = document.querySelector("textarea['comment-body']").value;
+
+
+
+
+
+
+};
