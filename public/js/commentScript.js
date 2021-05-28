1b04b482a043b18fe82d10901f59d8ca10434b71
Jason <jrosasrjf@gmail.com>
2021-05-27 20:35:17 -0500

    added in the huroku link to the readme

--- public/js/commentScript.js
+++ public/js/commentScript.js
@@ -5,9 +5,18 @@ const commentHandler = async function (event) {
     const postID = document.querySelector("input[name='post-id']").value;
     const postTextArea = document.querySelector("textarea['comment-body']").value;
 
+    //if the text body already exists
+    if (body) {
+        await fetch("../../controllers/api/comment-routes.js", {
 
+            // importing the information as an object. 
+            method: "POST", 
+            body:JSON.stringify({ postID, postTextArea }), // referencing the DOM variables 
+            headers: {"Content-Type": "application.json"}
+        });
 
-
-
-
+        document.location.reload(); // reloading the page afterwards 
+    }
 };
+
+// call the eventHandler above. Via click
