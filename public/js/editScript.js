f49938186794045b28158d19a3198158f171347c
Jason <jrosasrjf@gmail.com>
2021-05-31 16:47:47 -0500

    added in the basis of an edit script

---
+++ public/js/editScript.js
@@ -0,0 +1,14 @@
+// DOM Reference Variables 
+const postId; // document.querySelector(" ").value;
+
+/* -- Event Handlers -- */
+
+
+// Edit the text body
+const editTweet; 
+
+// Delete the post entirely
+const deleteTweet; 
+
+
+/* -- Call the Event Listeners */
