0d81c3ac70321e43dfa66fbff15568659c1e42a2
Jason <jrosasrjf@gmail.com>
2021-05-31 21:12:09 -0500

    used project 2 structure to finish signupScript

--- public/js/signupScript.js
+++ public/js/signupScript.js
@@ -1,19 +1,33 @@
 const { response } = require("express"); // auto populated when I used " if (response) "
 
 // where the magic happens
-const signupHandler; 
+const signupHandler = async function(event) {
+    event.preventDefault(); 
+
+    const usernameE1; // document.querySelector(" ");
+    const userPasswordE1; // document.querySelector(" ");
+
+    const response = await fetch("/api/user", {
+        method: "POST", // updates the current file. 
+        body: JSON.stringify({
+            username: usernameE1.value, 
+            password: userPasswordE1.value, 
+        }), 
+        headers: {"Content-Type": "application/json"}, 
+    }); 
+
+    /* -- Did all go well -- */
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        window.alert("Oh no! We couldn't sign you up."); 
+    }
+
+}; 
 
 
-// create sign up or show that there is a problem 
-if (response.ok) {
-    document.location.replace("/dashboard");
-} else {
-    window.alert("Oh no! We couldn't sign you up."); 
-}
+/* -- Event Handler -- */
+const signupE1; // document.querySelector(" "); 
 
-
-/* --Event Handler-- */
-const signupE1; // querySelector(" "); 
-
-//trigger 
-// signupE1.addEventListener("submit", signupHandler); 
+// event trigger 
+signupE1.addEventListener("submit", signupHandler); 
