9ad0966dbf6c1389fd9d16e6178f8c08cf63b96e
Jason <jrosasrjf@gmail.com>
2021-06-02 22:54:43 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,20 @@
 /*  creates the teather for the myDQL databases.  */ 
 
-// imports the sequalize library
-const Sequelize = require("sequelize"); 
-require("dotenv").config(); // imports DB values from the .env file. 
-
-const sequelize = new Sequelize(
-    // DB_ values from .env file
-    
-    process.env.DB_NAME, // will be TechBlogger_DB. 
-    process.env.DB_USER, // will be root. 
-    process.env.DB_PASSWORD, 
-
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
     {
-        host: "localhost",
-        dialect: "mysql", 
-        port: 3306, // unique port for db. 
-            // server is using 3008. 
-    }
-); 
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
 
 
 // exporting the object containing the DB information. 
