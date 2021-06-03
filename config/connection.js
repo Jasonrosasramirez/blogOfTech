787fc13eb0d6b679aeceb1d671e9a38e4cf085a7
Jason <jrosasrjf@gmail.com>
2021-06-03 17:17:02 -0500

    corrected connections.js with tutor

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,14 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
 /*  creates the teather for the myDQL databases.  */ 
 
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
+
+
+
+  const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
@@ -14,7 +18,7 @@ if (process.env.JAWSDB_URL) {
       port: 3306,
     },
   );
-}
+
 
 
 // exporting the object containing the DB information. 
